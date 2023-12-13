2ae578f55b58dbfa1c6ec6b089a2b8ae5a494ec0
Mehmet Alemdar <alemdarmehmet6@gmail.com>
2023-12-13 18:24:07 +0300

    Create user schema and dto

---
+++ src/user/schema/user.schema.ts
@@ -0,0 +1,24 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { HydratedDocument } from 'mongoose';
+
+export type UserDocument = HydratedDocument<User>;
+
+@Schema()
+export class User {
+  @Prop()
+  name: string;
+
+  @Prop()
+  email: string;
+
+  @Prop()
+  password: string;
+
+  @Prop()
+  age: number;
+
+  @Prop([String])
+  blogPosts: string[];
+}
+
+export const UserSchema = SchemaFactory.createForClass(User);
