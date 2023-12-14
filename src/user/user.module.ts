53636263382f48299612216a4a612176f5822ad4
Mehmet Alemdar <alemdarmehmet6@gmail.com>
2023-12-14 17:04:35 +0300

    Update user.module.ts

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,15 +1,14 @@
-import { Module, RequestMethod } from '@nestjs/common';
+import { Module, NestModule, MiddlewareConsumer} from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { UserController } from './controllers/user/user.controller';
 import { UserService } from './services/user/user.service';
 import { User, UserSchema } from './schema/user.schema';
+import { AuthenticationMiddleware } from '../middlewares/authentication/authentication.middleware';
 
 @Module({
   imports: [MongooseModule.forFeature([{ name: User.name, schema: UserSchema}])],
   controllers: [UserController],
   providers: [UserService]
 })
-export class UserModule {
-  
-
+export class UserModule{
 }
