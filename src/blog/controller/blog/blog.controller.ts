76d93cd0f5079c1300061b12c7fd028a9963cc02
Mehmet Alemdar <alemdarmehmet6@gmail.com>
2023-12-16 01:17:26 +0300

    Find blog by id

--- src/blog/controller/blog/blog.controller.ts
+++ src/blog/controller/blog/blog.controller.ts
@@ -1,6 +1,7 @@
-import { Controller, UsePipes, Get, ValidationPipe, Body } from '@nestjs/common';
+import { Controller, UsePipes, Get, ValidationPipe, Body, Param } from '@nestjs/common';
 import { BlogService } from 'src/blog/service/blog/blog.service';
 import { CreateBlogDto } from 'src/blog/dtos/createBlog.dto';
+import { Blog } from 'src/blog/schema/blog.schema';
 
 @Controller('blog')
 export class BlogController {
@@ -8,7 +9,17 @@ export class BlogController {
 
   @Get()
   @UsePipes(new ValidationPipe())
-  create(@Body() createBlogDto: CreateBlogDto): Promise<any> {
+  create(@Body() createBlogDto: CreateBlogDto): Promise<Blog> {
     return this.blogService.create(createBlogDto);
+  }
+
+  @Get('findAll')
+  findAll(): Promise<Blog[]> {
+    return this.blogService.findAll();
+  }
+
+  @Get('findOne/:id')
+  findOne(@Param("id") id: string): Promise<Blog> {
+    return this.blogService.findOne(id);
   }
 }
--- src/blog/service/blog/blog.service.ts
+++ src/blog/service/blog/blog.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, HttpException, HttpStatus } from '@nestjs/common';
+import { Injectable, HttpException, HttpStatus, Param } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { Blog } from '../../schema/blog.schema';
@@ -26,7 +26,14 @@ export class BlogService {
     } catch (error) {
       throw new HttpException("Someting went wrong", HttpStatus.BAD_REQUEST)
     }
-    // const createdBlog = new this.blogModel(createBlogDto);
-    // return createdBlog.save();
+  }
+
+  async findAll(): Promise<Blog[]> {
+    return this.blogModel.find().exec();
+  }
+
+  async findOne(id: string): Promise<Blog> {
+    console.log(id)
+    return await this.blogModel.findOne({ _id: id }).exec();
   }
 }
